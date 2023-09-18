1d2b23713fbfa333247c054425845162e96145be
rxMATTEO <erik_sundberg@mail.ru>
2023-09-18 13:05:13 +0300

    Add initial status

--- components/StatusSelect.vue
+++ components/StatusSelect.vue
@@ -6,12 +6,13 @@ const petStore = usePetStore();
 const statuses = ref(Object.values(petStore.getStatus));
 
 const emits = defineEmits(['change']);
-const selected = ref(null);
+const selected = ref(statuses.value[0]);
+emits('change', selected.value);
 </script>
 
 <template>
   <select v-model="selected" @change="emits('change', selected)">
-    <option v-for="status in statuses" :key="status as string">
+    <option v-for="(status) in statuses" :key="status as string">
       {{ status }}
     </option>
   </select>
