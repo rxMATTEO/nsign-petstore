af791376fb908f0da0ff71a1537ddc616a5cda4e
rxMATTEO <erik_sundberg@mail.ru>
2023-09-18 13:01:12 +0300

    Add status select and emits

---
+++ components/StatusSelect.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { usePetStore } from '~/stores/petStore.ts';
+
+const petStore = usePetStore();
+const statuses = ref(Object.values(petStore.getStatus));
+
+const emits = defineEmits(['change']);
+const selected = ref(null);
+</script>
+
+<template>
+  <select v-model="selected" @change="emits('change', selected)">
+    <option v-for="status in statuses" :key="status as string">
+      {{ status }}
+    </option>
+  </select>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import { usePetStore } from '~/stores/petStore.ts';
 
-const petStore = usePetStore();
-const statuses = ref(Object.values(petStore.getStatus));
+import StatusSelect from '~/components/StatusSelect.vue';
+
+function log (a) {
+  console.log(a);
+}
 </script>
 
 <template>
   <div>
-    <select>
-      <option v-for="status in statuses" :key="status">
-        {{ status }}
-      </option>
-    </select>
+    <StatusSelect @change="log" />
   </div>
 </template>
 
