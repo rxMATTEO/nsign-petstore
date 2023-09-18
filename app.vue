402a857aa8b7e1cb119f4fd0707b3f73cdf2f657
rxMATTEO <erik_sundberg@mail.ru>
2023-09-18 11:54:38 +0300

    Change nuxt-config

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="">
+    Hey, tailwind
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,52 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  devServer: {
-    port: parseInt(String(process.env.PORT)) && 3000
-  }
-})
+import { NuxtConfig } from "nuxt/config";
+
+const nuxtConfig: NuxtConfig = {
+  devtools: { enabled: true, },
+  buildModules: ['@nuxt/typescript-build'],
+  build: {
+    // @ts-ignore
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {}
+        }
+      }
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+  ],
+  pinia: {
+    autoImports: ['defineStore',
+      ['defineStore', 'definePiniaStore']
+    ]
+  },
+  vite: {
+    esbuild: {
+      drop: ['console', 'debugger']
+    }
+  },
+  runtimeConfig: {
+    public: {
+      API_URL: 'https://petstore.swagger.io/v2'
+    }
+  },
+  nitro: {
+    compressPublicAssets: true
+  },
+  app: {
+    head: {
+      meta: [
+        { name: 'description', content: 'Awesome pet store for Nsign.' }
+      ],
+      htmlAttrs: {
+        lang: 'en'
+      },
+    }
+  },
+};
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export default defineNuxtConfig(nuxtConfig);
